1d984bbb9e068c3bab771bb08e082bbd72dfdee4
Yugansh Marwah <ymarwah001@nam.ad.pwcinternal.com>
2017-11-14 07:11:26 -0500

    added setup.py file

---
+++ Models/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='models',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/yuganshmarwah/ModelTest.git',
+      author='yugansh',
+      author_email='yuganshmarwah@gmail.com',
+      license='AA',
+      packages=['funniest'],
+      zip_safe=False)
