5dae3cf0f96fd8e5a8458f71ce7ed8e6639ecf63
Yugansh Marwah <ymarwah001@nam.ad.pwcinternal.com>
2017-11-14 08:20:24 -0500

    packaged

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(name='models',
       version='0.1',
-      description='The funniest joke in the world',
-      url='https://github.com/yuganshmarwah/ModelTest.git',
+      description='Any description for models',
+      url='C:\Users\ymarwah001\git-package-test\ModelTest',
       author='yugansh',
       author_email='yuganshmarwah@gmail.com',
       license='AA',
       packages=['models'],
+	  install_requires=[
+          'jinja',
+      ],
       zip_safe=False)
